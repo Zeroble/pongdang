7d4b5b68d2ec36c570b653781bbf4d138f55a8eb
Zeroble <dlalsdyd0219@gmail.com>
2018-02-19 23:01:30 +0900

    asdf

---
+++ app/src/main/java/com/sup3rd3v3l0p3r/teamvetor/pongdang/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sup3rd3v3l0p3r.teamvetor.pongdang;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
